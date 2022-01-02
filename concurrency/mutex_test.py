b61a706bbf9a7242a8bbfe488ae3a3d2257513d6
jamesfrosty3150102 <james.frosty3150102@gmail.com>
2022-01-02 14:16:51 +0800

    add mutex t2

--- concurrency/mutex_test.py
+++ concurrency/mutex_test.py
@@ -9,6 +9,16 @@ class thread_one(threading.Thread):
         print("First thread is finished")
         mutex.release()
 
+class thread_two(threading.Thread):
+	def run(self):
+		global mutex
+		print ("The second thread is now sleeping")
+		time.sleep(random.randint(1, 5))
+		mutex.acquire()
+		print("Second thread is finished")
+
 mutex.acquire()
 t1=thread_one()
+t2 = thread_two()
 t1.start()
+t2.start()
