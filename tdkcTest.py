39a6ce9cd288e8c16d18f35e743beabdfbb48c40
jamesfrosty3150102 <james.frosty3150102@gmail.com>
2022-02-02 15:15:07 +0800

    the dark knight rises

--- tdkcTest.py
+++ tdkcTest.py
@@ -6,18 +6,26 @@ def parseString2(string):
     timeRule = re.compile(pattern)
     timeList= timeRule.findall(string)
     if timeList != None:
-        print(f"時間:{timeList}")
+        print(f"時間2:{timeList}")
 
 def parseString3(string):
     pattern = '\d:\d\d:\d\d'
     timeRule = re.compile(pattern)
     timeList= timeRule.findall(string)
     if timeList != None:
-        print(f"時間:{timeList}")
+        print(f"時間3:{timeList}")
 
+def parseString4(string):
+    pattern = 'The Dark Knight Rises'
+    timeRule = re.compile(pattern)
+    timeList= timeRule.findall(string)
+    if timeList != None:
+        print(f"TDKR:{timeList}")
 
 file_Obj = open(fn)
 data = file_Obj.read()
 parseString2(data)
 parseString3(data)
+parseString4(data)
+
 file_Obj.close()
