72352e34512f0d42002b3f39e66c2888b3a4a9c4
jamesfrosty3150102 <james.frosty3150102@gmail.com>
2022-02-02 15:12:00 +0800

    2/3 digits

--- tdkcTest.py
+++ tdkcTest.py
@@ -1,14 +1,23 @@
 import re
 fn = 'tdkc.txt'
 
-def parseString(string):
+def parseString2(string):
     pattern = '\d\d:\d\d'
     timeRule = re.compile(pattern)
     timeList= timeRule.findall(string)
     if timeList != None:
         print(f"時間:{timeList}")
 
+def parseString3(string):
+    pattern = '\d:\d\d:\d\d'
+    timeRule = re.compile(pattern)
+    timeList= timeRule.findall(string)
+    if timeList != None:
+        print(f"時間:{timeList}")
+
+
 file_Obj = open(fn)
 data = file_Obj.read()
-parseString(data)
+parseString2(data)
+parseString3(data)
 file_Obj.close()
