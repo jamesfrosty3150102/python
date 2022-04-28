d24fdeb58e15ab16f9cbbecedf3f811c7594d130
ywang43 <james.frosty3150102@gmail.com>
2022-04-28 21:41:37 +0800

    utf-8

---
+++ issues/len.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+james = [23, 19, 22, 31, 18]        # 定義james的5場比賽得分
+games = len(james)                  # 獲得場次數據  
+#print(f"經過 {games} 場比賽最高得分 = {max(james)}",encoding="utf-8")
+print(f"經過 {games} 場比賽最低得分 = {min(james)}")
+#print(f"經過 {games} 場比賽得分總計 = {sum(james)}")
